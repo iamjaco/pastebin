6b3aff2a0e5d43a0425e623c72ef6e5adf0cdfde
Iamjaco <54623676+iamjaco@users.noreply.github.com>
2023-04-14 04:54:19 +0000

    Add new feature

--- app.py
+++ app.py
@@ -1,28 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, abort
+from flask_bootstrap import Bootstrap
 import sqlite3
 from sqlite3 import Error
-from flask import g, Flask, render_template, request, redirect, url_for, abort
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
 
-pastebin = []
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('pastebin.db')
+    except Error as e:
+        print(e)
+
+    return conn
+
+def init_db():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS pastes (id INTEGER PRIMARY KEY, content TEXT NOT NULL)")
+    conn.commit()
+    conn.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         content = request.form['content']
-        pastebin.append(content)
-        return redirect(url_for('paste', index=len(pastebin)-1))
-    return render_template('index.html')
+        conn = create_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO pastes(content) VALUES (?)", (content,))
+        conn.commit()
+        paste_id = cursor.lastrowid
+        conn.close()
+        return redirect(url_for('paste', paste_id=paste_id))
 
-@app.route('/paste/<int:index>')
-def paste(index):
-    try:
-        content = pastebin[index]
-    except IndexError:
-        abort(404)  # Return a 404 error if the requested index does not exist
-    return render_template('paste.html', content=content, index=index)
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, content FROM pastes")
+    pastes = cursor.fetchall()
+    conn.close()
+
+    return render_template('index.html', pastes=pastes)
+
+    
+@app.route('/paste/<int:paste_id>')
+def paste(paste_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT content FROM pastes WHERE id=?", (paste_id,))
+    content = cursor.fetchone()
+    conn.close()
+
+    if content:
+        return render_template('paste.html', content=content[0], paste_id=paste_id)
+    else:
+        abort(404)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
